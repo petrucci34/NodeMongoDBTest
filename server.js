fa426113050098384df8adcf31b02413fea28be7
Korhan Bircan <korhan.bircan@capitalone.com>
2017-02-08 15:20:52 -0800

    Add database connection.

--- server.js
+++ server.js
@@ -1,14 +1,38 @@
 
-const express = require('express');
-const app = express();
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoClient = require('mongodb').MongoClient
+const app = express()
 const listeningPort = 3000
+var database
 
-console.log(__dirname)
+mongoClient.connect('mongodb://admin:admin-feb8-257@ds147599.mlab.com:47599/node-mongodb-test', (error, db) => {
+	if (error) {
+		console.log(error)
+		return 
+	}
+	
+	database = db
 
-app.listen(listeningPort, function() {
-	console.log('listening on ' + listeningPort)
+	app.listen(listeningPort, function() {
+		console.log('listening on ' + listeningPort)
+	})
 })
 
+app.use(bodyParser.urlencoded({extended: true}))
+
 app.get('/', (request, response) => {
-	response.send('Hello world!')
+	response.sendFile(__dirname + '/index.html')
+})
+
+app.post('/items', (request, response) => {
+	database.collection('items').save(request.body, (error, result) => {
+		if (error) {
+			console.log(error)
+			return 
+		}
+
+		console.log('Item saved to database.')
+		response.redirect('/')
+	})
 })
