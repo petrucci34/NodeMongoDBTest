d538e8ff2e9a25ac54fc705527bcbe29aab99f09
Korhan Bircan <korhan.bircan@capitalone.com>
2017-02-08 14:20:38 -0800

    Adding basic HTML index file.

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const express = require('express');
 const app = express();
 const listeningPort = 3000
 
+console.log(__dirname)
+
 app.listen(listeningPort, function() {
 	console.log('listening on ' + listeningPort)
+})
+
+app.get('/', (request, response) => {
+	response.send('Hello world!')
 })
